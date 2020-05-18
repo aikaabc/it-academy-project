9e087d453295ffda6c4c252a98a4fab4e41b437e
Aikokul14 <aikokul.aaz@gmail.com>
2020-05-18 20:43:01 +0600

    rendering

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Item
 
-# Create your views here.
+def item_list(request):
+    items = Item.published.all()
+    return render(request, 'blog/item/list.html', {'items': items})
+
+def item_detail(request, year, month, day, post):
+    item = get_object_or_404(Post, slug=post, status='published',publish__year=year,
+                        publish__month=month,publish__day=day)
+    return render(request,'blog/item/detail.html',{'item': item})
+    

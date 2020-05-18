6de1d88fe041c5b7086db1f6bdec25e96b701ae2
Aikokul14 <aikokul.aaz@gmail.com>
2020-05-18 20:38:41 +0600

    items saving

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+
 class Item(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -16,6 +21,8 @@ class Item(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
     
     class Meta:
         ordering = ('-publish',)
